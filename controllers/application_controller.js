f5a4597a6abffe3c6e5139a5062dcbd2656115d2
Jeff <Jeff>
2017-03-01 18:19:58 -0500

    notes added to application_controller

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -3,9 +3,25 @@ var express = require("express");
 // REQUIRED NPM END ------------------------------------------------------
 
 // REQUIRED FILES --------------------------------------------------------
-// I don't think this does anything. 
-// Waiting for Pavan
-var models = require("../models");
+// This is not required. We don't use it here.
+// Confirmed with Pavan
+// var models = require("../models");
+// REQUIRED FILES END ----------------------------------------------------
 
-
+// REQUIRED ROUTER -------------------------------------------------------
+// Creates a router object
+// **Link1: https://expressjs.com/en/api.html#express.router**
 var router = express.Router();
+// REQUIRED ROUTER END ---------------------------------------------------
+
+
+// router.METHOD(path, [callback, ...] callback)
+// The router.METHOD() methods provide the routing functionality in Express, 
+// where METHOD is one of the HTTP methods, such as GET, PUT, POST, and so on, 
+// in lowercase. Thus, the actual methods are router.get(), router.post(), 
+// router.put(), and so on.
+// **Link1: https://expressjs.com/en/api.html#router**
+
+
+// Export router
+module.exports = router;
